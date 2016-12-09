7ff3505ec2d5e9db7a0726be6e37deb091d0eca2
huabin.zhao <z8156549@163.com>
2016-12-09 15:33:08 +0800

    add nyc & mocha

---
+++ dist/index.d.ts
@@ -0,0 +1,15 @@
+export declare class RuleEngine {
+    ignoreFactChanges: any;
+    activeRules: any;
+    rules: any;
+    constructor(rules?: any, options?: any);
+    init(): void;
+    register(rules: any): void;
+    sync(): void;
+    findRules(filter?: any): any;
+    turn(state: any, filter: any): void;
+    prioritize(priority: any, filter: any): void;
+    toJSON(): any;
+    fromJSON(rules: any): void;
+    execute(fact: any, cb: any): void;
+}
